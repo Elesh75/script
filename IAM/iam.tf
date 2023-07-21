f72e8a47127cbada17f11ef2dad8851f94f7b3ab
elesh75 <oguntademujaheed2@gmail.com>
2023-07-21 23:16:02 +0100

    3rd

--- IAM/iam.tf
+++ IAM/iam.tf
@@ -10,10 +10,15 @@ resource "aws_iam_user" "Tunde" {
   }
 }
 
+#This code block creates an access key for the IAM user "Tunde". 
+#The aws_iam_user.Tunde.name expression references the name attribute of the aws_iam_user resource created earlier.
 resource "aws_iam_access_key" "Tunde" {
   user = aws_iam_user.Tunde.name
 }
 
+#This code block creates an IAM policy document that allows the "Tunde" user to assume any role. 
+#It uses the aws_iam_policy_document data source to define the policy document. In this case, 
+#it allows the "sts:AssumeRole" action on any resource ("*").
 data "aws_iam_policy_document" "Tunde_ro" {
   statement {
     effect    = "Allow"
@@ -22,6 +27,9 @@ data "aws_iam_policy_document" "Tunde_ro" {
   }
 }
 
+# This code block attaches the IAM policy document defined in the previous step to the IAM user "Tunde". 
+#It creates an IAM user policy named "test" and associates it with the "Tunde" user. 
+#The policy attribute references the JSON representation of the policy document defined in the data.aws_iam_policy_document.Tunde_ro data source.
 resource "aws_iam_user_policy" "Tunde_ro" {
   name   = "test"
   user   = aws_iam_user.Tunde.name
